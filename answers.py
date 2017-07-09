8630f585502e625b12a8be9b45616ce37f3a1467
buymercier <corentin.mercier@epita.fr>
2017-07-09 13:49:58 +0200

    2nd question done

--- answers.py
+++ answers.py
@@ -34,15 +34,20 @@ def bst_to_list(T, l = []):
 # 1.2 Write a function that builds a balanced binary search tree using a sorted
 # list.
 
-def list_to_avl(l, B = BinTree(None, None, None)):
-    if len(l) == 0:
-        return None
-    else:
-        B.key = l[len(l) // 2]
-        l.pop(len(l) // 2)
-        if len(l) > 0:
-            B.left = list_to_avl(l[:(len(l) // 2):])
-            B.right = list_to_avl(l[(len(l) // 2)::])
+def listToAVL (L, l, r):
+	if l < r:
+		m = (l+r)//2
+		T = BinTree(L[m], None, None)
+		T.left = listToAVL(L, l, m)
+		T.right = listToAVL(L, m+1,r)
+		return T
+
+def list_to_avl(l):
+    if len(l) != 0:
+        B = BinTree(None, None, None)
+        B.key = l.pop(len(l) // 2)
+        B.left = list_to_avl(l[:(len(l) // 2):])
+        B.right = list_to_avl(l[(len(l) // 2)::])
         return B
 
 ################################################################################
@@ -53,7 +58,7 @@ D = BinTree(4, None, None)
 B = BinTree(3, None, D)
 A = BinTree(7, B, C)
 
-l = bst_to_list(A)
-print(l)
-tree = list_to_avl(l)
+# tree = list_to_avl(list("123456789"), 0, 9)
+tree = list_to_avl(list("123456789"))
+print(tree.key)
 toSVG(tree, "bst")
